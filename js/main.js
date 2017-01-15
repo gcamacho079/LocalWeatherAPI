a98ca09ca3d892a7abe9812ce88405f31c706432
Guadalupe Camacho <gcamacho079@gmail.com>
2017-01-14 16:04:27 -0800

    Change APIs to https

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ var apiKey = 'c4a1d2161527b3e5034c822be0331fe8'; // API key for OpenWeather API
 
 $(document).ready(function() {
   // Obtains city and country codes
-  $.getJSON("http://geoip.nekudo.com/api/en/", function(json) {
+  $.getJSON("https://geoip.nekudo.com/api/en/", function(json) {
     var city = JSON.stringify(json.city);
     var countryCode = JSON.stringify(json.country.code);
     cityUnquote = city.substring(1, city.length - 1);
     $('#city').html(cityUnquote); //Removes quotes from city name
 
     // Obtains weather data and prints to page
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + ',' + countryCode + '&units=imperial&APPID=' + apiKey, function(json) {
+    $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + city + ',' + countryCode + '&units=imperial&APPID=' + apiKey, function(json) {
       var tempF = json.main.temp;
       var tempC = (tempF - 32.0)*(5/9);
       tempC = tempC.toFixed(2);
